8743410293f90af9b2f3538a8d45ee750b1a415b
prem <premkumar.bhaskal@nokia.com>
2017-11-21 19:49:21 +0530

    started with new week4 new way to use regularized theta by zeroing the 1st index of actual theta. see lrCostFunction.m for that.

---
+++ week4/lrCostFunction.m
@@ -0,0 +1,64 @@
+function [J, grad] = lrCostFunction(theta, X, y, lambda)
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%regularization
+%   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Hint: The computation of the cost function and gradients can be
+%       efficiently vectorized. For example, consider the computation
+%
+%           sigmoid(X * theta)
+%
+%       Each row of the resulting matrix will contain the value of the
+%       prediction for that example. You can make use of this to vectorize
+%       the cost function and gradient computations. 
+%
+% Hint: When computing the gradient of the regularized cost function, 
+%       there're many possible vectorized solutions, but one solution
+%       looks like:
+%           grad = (unregularized gradient for logistic regression)
+%           temp = theta; 
+%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           grad = grad + YOUR_CODE_HERE (using the temp variable)
+%
+
+
+
+% computing cost function with regularized param
+
+hthetaX = sigmoid(X * theta); % X[m*n] * theta[n*1]
+JTerm1 = y' * log(hthetaX);% size m * 1
+JTerm2 = (1 .- y)' * log(1 .- hthetaX);% [1,m] * [m,1]
+
+% removing theta(0) so that it is nt
+regTheta = theta; % size [n * 1]
+regTheta(1,1) = 0;
+JTermReg = lambda/(2 * m) * (regTheta' * regTheta);
+
+J = (-1/m) * (JTerm1 + JTerm2) + JTermReg; % scalar value
+
+% computing grad function
+gradTerm1 = 1/m .* (X' * (hthetaX .- y)); % [nx1] vector
+gradRegTerm = (lambda/m) .* regTheta; % [n x 1] vector
+
+grad = gradTerm1 .+ gradRegTerm;
+
+% =============================================================
+
+grad = grad(:);
+
+end
---
+++ week4/predictOneVsAll.m
@@ -0,0 +1,46 @@
+function p = predictOneVsAll(all_theta, X)
+%PREDICT Predict the label for a trained one-vs-all classifier. The labels 
+%are in the range 1..K, where K = size(all_theta, 1). 
+%  p = PREDICTONEVSALL(all_theta, X) will return a vector of predictions
+%  for each example in the matrix X. Note that X contains the examples in
+%  rows. all_theta is a matrix where the i-th row is a trained logistic
+%  regression theta vector for the i-th class. You should set p to a vector
+%  of values from 1..K (e.g., p = [1; 3; 1; 2] predicts classes 1, 3, 1, 2
+%  for 4 examples) 
+
+m = size(X, 1);
+num_labels = size(all_theta, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned logistic regression parameters (one-vs-all).
+%               You should set p to a vector of predictions (from 1 to
+%               num_labels).
+%
+% Hint: This code can be done all vectorized using the max function.
+%       In particular, the max function can also return the index of the 
+%       max element, for more information see 'help max'. If your examples 
+%       are in rows, then, you can use max(A, [], 2) to obtain the max 
+%       for each row.
+%       
+
+% for each input x, we find probability value h_theta(x) = g(theta.X) for each of the labels.
+% then find which label has max probability, then we predict X resemebles that label
+temp_term = X * all_theta'; % size [m,n+1] x [k,n+1]' = [m,k]  each row is h_theta(x(i)).
+hThetaX = sigmoid(temp_term); % each row is for each input x, each column is probability value for each label k.
+
+% find which label has max probability
+[val, label] = max(hThetaX,[],2); % find rowwise max
+
+p = label;
+
+% =========================================================================
+
+
+end
