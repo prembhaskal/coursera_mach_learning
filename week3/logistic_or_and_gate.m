2be231ed45d394afd5001ddf3a4fc96035c65768
prem <premkumar.bhaskal@nokia.com>
2017-11-21 12:48:39 +0530

    found that for complex functions like xnor etc, 2 features are not enough to map output. feature mapping produces better results.
    also printing hypothesis function h(x) = g(theta * X) for each logical gate showing that output is correct.

--- week3/logistic_or_and_gate.m
+++ week3/logistic_or_and_gate.m
@@ -41,7 +41,9 @@ function logistic_or_and_gate ()
   fprintf('theta from fminunc is \n');
   fprintf(' %f\n', theta);
   fprintf("********************************************\n\n");
-
+  
+  fprintf('printing hypothesis function for or gate values\n');
+  fprintf('%d %d %1.2f\n',[X(:,[2:3]), sigmoid(X * theta)]'); % this prints values side by side.
   fprintf("calculating theta values for AND gate\n\n");
   
   X = [0 0; 0 1; 1 0; 1 1;];
@@ -58,6 +60,61 @@ function logistic_or_and_gate ()
   fprintf('cost from fminunc is %f\n', cost);
   fprintf('theta from fminunc is \n');
   fprintf(' %f\n', theta);
+  
+  fprintf('printing hypothesis function for AND gate values\n');
+  fprintf('%d %d %1.2f\n',[X(:,[2:3]), sigmoid(X * theta)]');
   fprintf("********************************************\n\n");
+
+
+  fprintf("calculating theta values for XNOR gate\n\n");
+  fprintf("logistic regression with only 2 variable fails to find satisfactory answer with just 2 variables");
+  
+  X = [0 0; 0 1; 1 0; 1 1;];
+  y = not(xor(X(:,1), X(:,2)));
+  X = [ones(size(X,1),1) X];
+  theta = zeros(size(X,2),1);
+  
+  % optimize using fminunc
+  options = optimset('GradObj', 'on', 'MaxIter', 400);
+  
+  costFunctionTheta = @(theta)(costFunction(theta, X, y));
+  [theta, cost] = fminunc(costFunctionTheta, theta, options);
+  
+  fprintf('cost from fminunc is %f\n', cost);
+  fprintf('theta from fminunc is \n');
+  fprintf(' %f\n', theta);
+  
+  fprintf('hypothesis function of XNOR without feature mapping.\n');
+  fprintf('%d %d %1.2f\n',[X(:,[2:3]), sigmoid(X * theta)]');
+  
+  fprintf("********************************************\n\n");
+  
+  
+  fprintf("calculating theta values for XNOR gate with feature mapping to degree 2\n\n");
+  
+  X = [0 0; 0 1; 1 0; 1 1;];
+  y = not(xor(X(:,1), X(:,2)));
+  
+  N = 2;
+  X = mapFeatureN(X(:,1), X(:,2), N);
+  fprintf('feature count after the mapping to degree %d is %d\n', N, size(X,2));
+  theta = zeros(size(X,2),1);
+    
+  % optimize using fminunc
+  options = optimset('GradObj', 'on', 'MaxIter', 400);
+  
+  lambda = 0;
+  costFunctionRegT = @(theta)(costFunctionReg(theta, X, y, lambda));
+  [theta, cost] = fminunc(costFunctionRegT, theta, options);
+  
+  fprintf('cost from fminunc is %f\n', cost);
+  fprintf('theta from fminunc is \n');
+  fprintf(' %f\n', theta);
+  
+  fprintf('hypothesis function of XNOR with feature mapping.\n');
+  fprintf('%d %d %1.2f\n',[X(:,[2:3]), sigmoid(X * theta)]');
+  fprintf("********************************************\n\n");
+  
+  
 
 endfunction
